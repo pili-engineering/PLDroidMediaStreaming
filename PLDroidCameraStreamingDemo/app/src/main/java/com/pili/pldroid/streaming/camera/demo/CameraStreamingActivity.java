b260913974250a13f4d1e2deefb53f8e7e0f4cba
jpxiong <xiongjipu@gmail.com>
2015-08-21 19:36:47 +0800

    release v1.3.0

--- PLDroidCameraStreamingDemo/app/src/main/java/com/pili/pldroid/streaming/camera/demo/CameraStreamingActivity.java
+++ PLDroidCameraStreamingDemo/app/src/main/java/com/pili/pldroid/streaming/camera/demo/CameraStreamingActivity.java
@@ -1,18 +1,31 @@
 package com.pili.pldroid.streaming.camera.demo;
 
+import android.content.Context;
+import android.graphics.Bitmap;
 import android.hardware.Camera;
 import android.opengl.GLSurfaceView;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
 import android.util.Log;
 import android.view.View;
+import android.view.Window;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.pili.pldroid.streaming.CameraStreamingManager;
 import com.pili.pldroid.streaming.CameraStreamingSetting;
+import com.pili.pldroid.streaming.FrameCapturedCallback;
 import com.pili.pldroid.streaming.StreamingProfile;
 import com.pili.pldroid.streaming.StreamingProfile.Stream;
 import com.pili.pldroid.streaming.widget.AspectFrameLayout;
+
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Map;
 
 /**
  * Created by jerikc on 15/6/16.
@@ -23,33 +36,48 @@ public class CameraStreamingActivity extends StreamingBaseActivity {
     private Button mTorchBtn;
     private boolean mIsTorchOn = false;
     private Button mCameraSwitchBtn;
+    private Button mCaptureFrameBtn;
+    private StreamingProfile mProfile;
+    private Map<Integer, Integer> mSupportVideoQualities;
+    private Context mContext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+            requestWindowFeature(Window.FEATURE_ACTION_BAR_OVERLAY);
+        } else {
+            requestWindowFeature(Window.FEATURE_NO_TITLE);
+        }
         super.onCreate(savedInstanceState);
+        mContext = this;
 
         setContentView(R.layout.activity_camera_streaming);
 
         AspectFrameLayout afl = (AspectFrameLayout) findViewById(R.id.cameraPreview_afl);
+        afl.setShowMode(AspectFrameLayout.SHOW_MODE.FULL);
         GLSurfaceView glSurfaceView = (GLSurfaceView) findViewById(R.id.cameraPreview_surfaceView);
+
         mShutterButton = (Button) findViewById(R.id.toggleRecording_button);
 
         mSatusTextView = (TextView) findViewById(R.id.streamingStatus);
         mTorchBtn = (Button) findViewById(R.id.torch_btn);
         mCameraSwitchBtn = (Button) findViewById(R.id.camera_switch_btn);
+        mCaptureFrameBtn = (Button) findViewById(R.id.capture_btn);
 
         Stream stream = new Stream(mJSONObject);
-        StreamingProfile profile = new StreamingProfile();
-        profile.setVideoQuality(StreamingProfile.VIDEO_QUALITY_MEDIUM1)
-                .setAudioQuality(StreamingProfile.AUDIO_QUALITY_HIGH2)
+        mProfile = new StreamingProfile();
+        mProfile.setVideoQuality(StreamingProfile.VIDEO_QUALITY_LOW1)
+                .setAudioQuality(StreamingProfile.AUDIO_QUALITY_HIGH1)
+                .setSendingBufferProfile(new StreamingProfile.SendingBufferProfile(0.2f, 0.8f, 3.0f, 20 * 1000))
                 .setStream(stream);
+        mSupportVideoQualities = mProfile.getSupportVideoQualities();
 
         CameraStreamingSetting setting = new CameraStreamingSetting();
         setting.setCameraId(Camera.CameraInfo.CAMERA_FACING_BACK)
                 .setContinuousFocusModeEnabled(true)
-                .setStreamingProfile(profile)
+                .setStreamingProfile(mProfile)
                 .setCameraPrvSizeLevel(CameraStreamingSetting.PREVIEW_SIZE_LEVEL.MEDIUM)
-                .setCameraPrvSizeRatio(CameraStreamingSetting.PREVIEW_SIZE_RATIO.RATIO_4_3);
+                .setCameraPrvSizeRatio(CameraStreamingSetting.PREVIEW_SIZE_RATIO.RATIO_16_9);
 
         mCameraStreamingManager = new CameraStreamingManager(this, afl, glSurfaceView);
         mCameraStreamingManager.onPrepare(setting);
@@ -95,6 +123,22 @@ public class CameraStreamingActivity extends StreamingBaseActivity {
                 mHandler.postDelayed(mSwitcher, 100);
             }
         });
+
+        mCaptureFrameBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mHandler.removeCallbacks(mScreenshooter);
+                mHandler.postDelayed(mScreenshooter, 100);
+            }
+        });
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mHandler.removeCallbacksAndMessages(null);
+        mSwitcher = null;
+        mScreenshooter = null;
     }
 
     private Switcher mSwitcher = new Switcher();
@@ -102,6 +146,60 @@ public class CameraStreamingActivity extends StreamingBaseActivity {
         @Override
         public void run() {
             mCameraStreamingManager.switchCamera();
+        }
+    }
+
+    private Screenshooter mScreenshooter = new Screenshooter();
+    private class Screenshooter implements Runnable {
+        @Override
+        public void run() {
+            final String fileName = "PLStreaming_" + System.currentTimeMillis() + ".jpg";
+            mCameraStreamingManager.captureFrame(540, 960, new FrameCapturedCallback() {
+                private Bitmap bitmap;
+
+                @Override
+                public void onFrameCaptured(Bitmap bmp) {
+                    bitmap = bmp;
+                    new Thread(new Runnable() {
+                        @Override
+                        public void run() {
+                            try {
+                                saveToSDCard(fileName, bitmap);
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            } finally {
+                                if (bitmap != null) {
+                                    bitmap.recycle();
+                                    bitmap = null;
+                                }
+                            }
+                        }
+                    }).start();
+                }
+            });
+        }
+    }
+
+    public void saveToSDCard(String filename, Bitmap bmp) throws IOException {
+        if(Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)) {
+            File file = new File(Environment.getExternalStorageDirectory(), filename);
+            BufferedOutputStream bos = null;
+            try {
+                bos = new BufferedOutputStream(new FileOutputStream(file));
+                bmp.compress(Bitmap.CompressFormat.PNG, 90, bos);
+                bmp.recycle();
+                bmp = null;
+            } finally {
+                if (bos != null) bos.close();
+            }
+
+            final String info = "Save frame to:" + Environment.getExternalStorageDirectory().getAbsolutePath() + "/" + filename;
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    Toast.makeText(mContext, info, Toast.LENGTH_LONG).show();
+                }
+            });
         }
     }
 
@@ -140,4 +238,19 @@ public class CameraStreamingActivity extends StreamingBaseActivity {
         }
     }
 
+    @Override
+    public boolean onStateHandled(final int state, Object extra) {
+        super.onStateChanged(state, extra);
+        switch (state) {
+            case CameraStreamingManager.STATE.SENDING_BUFFER_HAS_FEW_ITEMS:
+                mProfile.improveVideoQuality(1);
+                mCameraStreamingManager.notifyProfileChanged(mProfile);
+                return false;
+            case CameraStreamingManager.STATE.SENDING_BUFFER_HAS_MANY_ITEMS:
+                mProfile.reduceVideoQuality(1);
+                mCameraStreamingManager.notifyProfileChanged(mProfile);
+                return false;
+        }
+        return false;
+    }
 }
